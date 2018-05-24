79d5a9ad7c5369df3285b1eb8ca8fc622dd0c2b2
femap <28697785+feMap@users.noreply.github.com>
2018-05-25 01:10:42 +0800

    with Ajax

---
+++ docs/scripts/main.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(function(){  
+	$(".userBusinessRoom").hover(function(){  
+		$(".userBusinessRoom_content").show();
+	},function(){  
+		$(".userBusinessRoom_content").hide();
+	})  
+})  
